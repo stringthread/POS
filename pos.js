83d61ff6e093aa4650c7fddf7bdf6b8b048ed58e
stringthread <string.thread@gmail.com>
2017-06-19 20:00:38 +0900

    changes

--- pos.js
+++ pos.js
@@ -9,7 +9,8 @@ var is_barcode=false;
 var ctr=0;
 var total=0;
 var select=0;
-const ID_BARCODE=";";
+var nth=new Date().getFullYear()-1951;
+const ID_BARCODE="Tab";
 
 var init=function(){
   container=document.getElementById('container');
@@ -40,7 +41,7 @@ var init=function(){
     break;
     case 1:
     subtitle.innerHTML="商品入力";
-    container.innerHTML="<div id=\"item_container\"><div id=\"item_total\">合計金額 : &yen;0</div></div>";
+    container.innerHTML="<div id=\"item_container\"><div id=\"item_total\">合計 : &yen;0</div></div>";
     document.getElementById("method").innerHTML="Shift+Enterで次の画面<br>Escapeで前の画面<br>Backspaceで個数を1桁消去<br>Deleteキーで品目を削除<br>上下キーで品目を選択<br><br>商品のバーコードを読み込んだ後、数字キーで個数を入力";
     document.getElementById("receipt").innerHTML="";
     document.removeEventListener('keydown',deco_input);
@@ -48,14 +49,15 @@ var init=function(){
     break;
     case 2:
     subtitle.innerHTML="商品確認";
-    container.innerHTML="<div id=\"item_container\"><div id=\"item_total\">合計金額 : &yen;0</div></div>";
+    container.innerHTML="<div id=\"item_container\"><div id=\"item_total\">合計 : &yen;0</div></div>";
     document.getElementById("method").innerHTML="Shift+Enterで確定<br>Escapeで前の画面";
     document.getElementById("receipt").innerHTML="";
     item_draw();
     document.removeEventListener('keydown',item_input);
     break;
     case 3:
-    subtitle.innerHTML="文実販売<div id=\"receipt_caption\">レシート</div>";
+    subtitle.innerHTML="文実販売";
+    document.getElementById("receipt_caption").innerHTML="筑波大学附属駒場中高<br>第"+nth+"回文化祭実行委員会";
     container.innerHTML="<div id=\"item_container\"></div>";
     document.getElementById("footer").innerHTML="";
     item_draw();
@@ -78,7 +80,7 @@ var fin=function(){
     }
     break;
     case 1:
-    if(!item[0])return false;
+    if(!item[0][0] in item_table || item[0][4]==0)return false;
     break;
   }
   return true;
@@ -106,6 +108,7 @@ var deco_input=function(e){
   if(is_barcode&&isFinite(e.key)){
     tmp_int=tmp_int*10+parseInt(e.key);
   }
+  e.preventDefault();
 }
 
 var item_input=function(e){
@@ -121,7 +124,6 @@ var item_input=function(e){
   if(e.key=="Backspace"){
     if(!is_barcode){
       item[select][3]=Math.floor(item[select][3]/10);
-      tmp_int=Math.floor(tmp_int/10);
       item_draw();
     }
   }
@@ -129,11 +131,21 @@ var item_input=function(e){
     if(is_barcode){
       item[ctr][0]=tmp_int;
       tmp_int=0;
-    }else if(select<=ctr&&ctr!=0){
+      if(item[ctr][0] in item_table){
+        item[ctr][1]=item_table[item[ctr][0]][1];
+        item[ctr][2]=item_table[item[ctr][0]][2];
+        item[ctr][4]=item[ctr][2]*item[ctr][3];
+      }else{
+        window.alert("バーコードの読み取りに失敗しました。読み直してください");
+        item.splice(select,1);
+        if(ctr>0)ctr--;
+        if(item.length==0)item[0]=[0,"",0,0,0];
+      }
+    }else if(select<=ctr&&ctr>=0){
       item[select][3]=tmp_int;
       tmp_int=0;
+      item_append();
     }
-    item_append();
     item_draw();
     is_barcode=!is_barcode;
   }
@@ -147,12 +159,23 @@ var item_input=function(e){
     if(select<0)select=0;
     item_draw();
   }
+  if(e.key=="ArrowRight"){
+    item[select][3]++;
+    item_draw();
+  }
+  if(e.key=="ArrowLeft"){
+    if(item[select][3]>=1){
+      item[select][3]--;
+      item_draw();
+    }
+  }
   if(e.key=="Delete"){
     item.splice(select,1);
     if(ctr>0)ctr--;
     if(item.length==0)item[0]=[0,"",0,0,0];
     item_draw();
   }
+  e.preventDefault();
 };
 
 var item_draw=function(){
@@ -164,37 +187,27 @@ var item_draw=function(){
     inner+="<div class=\"item\"";
     if(mode==1&&key==select)inner+=" style=\"background-color:#9999ff\"";
     inner+="><div class=\"item_name\">";
-    inner+=item[key][1];
-    inner+="</div><div class=\"item_price,print_hidden\">&yen";
+    inner+="<small>"+item[key][0]+"</small>&emsp;"+item[key][1];
+    inner+="</div>\r\n<div class=\"item_price\">&yen";
     inner+=item[key][2];
     inner+="</div><div class=\"item_num\">&times";
-    inner+=item[key][3]
-    inner+="</div><div class=\"item_subtotal\">小計&nbsp&yen";
-    inner+=item[key][4];
+    inner+=(item[key][3]==0)?"(1)":item[key][3];
+    inner+="</div><div class=\"item_subtotal\">&yen";
+    inner+=(item[key][4]==0)?item[key][2]:item[key][4];
     inner+="</div></div>";
-    total+=item[key][4];
+    total+=(item[key][4]==0)?item[key][2]:item[key][4];
   }
-  inner+="<div id=\"item_total\">合計金額 : &yen;";
+  inner+="<div id=\"item_total\">合計&nbsp;:&nbsp;&yen;";
   inner+=total;
   inner+="</div>";
   item_container.innerHTML=inner;
 }
 
 var item_append=function(){
-  if(item[ctr][0] in item_table){
-    item[ctr][1]=item_table[item[ctr][0]][1];
-    item[ctr][2]=item_table[item[ctr][0]][2];
-    item[ctr][4]=item[ctr][2]*item[ctr][3];
-    if(item[ctr][3]){
-      ctr++;
-      select=ctr;
-      item[ctr]=[0,"",0,0,0];
-    }
-  }else if(item[ctr][0]!=0){
-    window.alert("バーコードの読み取りに失敗しました。読み直してください");
-    item.splice(select,1);
-    if(ctr>0)ctr--;
-    if(item.length==0)item[0]=[0,"",0,0,0];
+  if(item[ctr][0] in item_table&&item[ctr][3]>=0){
+    ctr++;
+    select=ctr;
+    item[ctr]=[0,"",0,0,0];
   }
 };
 
@@ -207,7 +220,7 @@ var receipt_draw=function(){
   tmp_str+=total.toLocaleString();
   tmp_str+="&ndash;</div><div id=\"receipt_proviso\">但し、文実販売代金として</div><div id=\"receipt_date\">発行日 : ";
   tmp_str+=getNow();
-  tmp_str+="</div><div id=\"receipt_pub\">筑波大附属駒場中高文化祭実行委員会</div><div id=\"receipt_stamp\">印</div>"
+  tmp_str+="</div><div id=\"receipt_pub\">筑波大附属駒場中高<br>第"+nth+"回文化祭実行委員会</div><div id=\"receipt_stamp\">印</div>"
   document.getElementById("receipt").innerHTML=tmp_str;
   tmp_str="";
 };
@@ -248,6 +261,7 @@ var sendDeal=function(){
   var tmp_arr=[[]];
   for (var key in item) {
     var item_code=item[key][0];
+    if(item_code==0)continue;
     tmp_arr[key]=[item_table[item_code][0],item[key][3]];
   }
   var data="mode=deal&serial=";
